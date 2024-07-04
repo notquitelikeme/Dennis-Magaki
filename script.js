d8325bc604aac581312c187fb38107a30e5700d8
notquitelikeme <conquistador8240@gmail.com>
2024-07-04 19:51:10 +0300

    Second Release of the portfolio

--- script.js
+++ script.js
@@ -4,21 +4,48 @@ const translations = {
         title: "Junior Developer",
         welcome: "Welcome to my web development portfolio",
         description: "I specialise in web design with responsive modern design,<br>Working with the latest technologies and development tools.",
-        nav: ["Home", "My Projects", "Technical Skills", "Services", "Contact Me"]
+        nav: ["Home", "Technical Skills", "My Projects", "Services", "Contact Me"],
+        secttech: "Technical Skills",
+        secttech1: "Back-end",
+        secttech2: "Front-end",
+        secttech3: "Databases",
+        secttech4: "Version Control",
+        secttech5: "Deployment",
+        skill1: "Some experience with NodeJS",
+        skill2: "Some experience with BootstrapCSS",
+        skill3: "Some experience with React"
     },
     Swahili: {
         greeting: "Jambo, ninaitwa Dennis",
         title: "Msanidi Mdogo",
         welcome: "Karibu kwenye tovuti langu la usanidi",
         description: "Nina utaalamu wa muundo wa wavuti na muundo wa kisasa unaotajika,<br>Ninafanya kazi na teknolojia za kisasa na zana za maendeleo.",
-        nav: ["Mwanzo", "Miradi Yangu", "Ujuzi wa Kiufundi", "Huduma", "Wasiliana Nami"]
+        nav: ["Mwanzo", "Ujuzi wa Kiufundi", "Miradi Yangu", "Huduma", "Wasiliana Nami"],
+        secttech: "Ujuzi wa Kiufundi",
+        secttech1: "Ujenzinyuma",
+        secttech2: "Ujenzimbele",
+        secttech3: "Hifadhidata",
+        secttech4: "Udhibiti Toleo",
+        secttech5: "Usambazaji",
+        skill1: "Tajriba kiasi kwenye NodeJS",
+        skill2: "Tajriba kiasi kwenye BootstrapCSS",
+        skill3: "Tajriba kiasi kwenye React"
     },
     German: {
         greeting: "Hallo, ich heiße Dennis",
         title: "Junior Entwickler",
         welcome: "Willkommen in meinem Webentwicklungsportfolio",
         description: "Ich spezialisiere mich auf Webdesign mit responsivem modernen Design,<br>Arbeiten mit den neuesten Technologien und Entwicklungstools.",
-        nav: ["Heim", "Meine Projekte", "Technische Fähigkeiten", "Dienstleistungen", "Kontaktieren Sie mich"]
+        nav: ["Heim", "Technische Fähigkeiten", "Meine Projekte", "Dienstleistungen", "Kontaktieren Sie mich"],
+        secttech: "Technische Fähigkeiten",
+        secttech1: "Backend",
+        secttech2: "Frontend",
+        secttech3: "Datenbank",
+        secttech4: "Versionskontrolle",
+        secttech5: "Einsatz",
+        skill1: "Einige Erfahrung mit NodeJS",
+        skill2: "Einige Erfahrung mit BootstrapCSS",
+        skill3: "Einige Erfahrung mit React"
     }
 };
 
@@ -48,6 +75,15 @@ document.querySelectorAll('.custom-option').forEach(option => {
         document.getElementById('title').innerHTML = translations[selectedLanguage].title;
         document.getElementById('welcome').innerHTML = translations[selectedLanguage].welcome;
         document.getElementById('description').innerHTML = translations[selectedLanguage].description;
+        document.getElementById('secttech').innerHTML = translations[selectedLanguage].secttech;
+        document.getElementById('secttech1').innerHTML = translations[selectedLanguage].secttech1;
+        document.getElementById('secttech2').innerHTML = translations[selectedLanguage].secttech2;
+        document.getElementById('secttech3').innerHTML = translations[selectedLanguage].secttech3;
+        document.getElementById('secttech4').innerHTML = translations[selectedLanguage].secttech4;
+        document.getElementById('secttech5').innerHTML = translations[selectedLanguage].secttech5;
+        document.getElementById('skill1').innerHTML = translations[selectedLanguage].skill1;
+        document.getElementById('skill2').innerHTML = translations[selectedLanguage].skill2;
+        document.getElementById('skill3').innerHTML = translations[selectedLanguage].skill3;
 
         // Update nav links
         const navItems = document.querySelectorAll('#nav li a');
