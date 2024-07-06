7986a010416c8dc42136c8c05b5bd0cd86e91a97
notquitelikeme <conquistador8240@gmail.com>
2024-07-06 19:41:45 +0300

    Third release of the portfolio

--- script.js
+++ script.js
@@ -13,7 +13,16 @@ const translations = {
         secttech5: "Deployment",
         skill1: "Some experience with NodeJS",
         skill2: "Some experience with BootstrapCSS",
-        skill3: "Some experience with React"
+        skill3: "Some experience with React",
+        sectproj: "My Projects",
+        ptitle1: "Kicks.Co E-Commerce Website",
+        ptitle2: "GitHub Widgets",
+        pp1: "This is an E-Commerce website prototype that specializes in selling sneakers, otherwise known as 'Kicks' in slang vocabulary.<br><br>FrontEnd: HTML, CSS, JS <br>BackEnd: JS, Firestore Firebase",
+        pp2: "This is an unofficial GitHub Widget Service that allows one to embed their GitHub Profile and GitHub Repositories information on their sites.<br><br>FrontEnd: HTML, CSS, JS <br>BackEnd: JS</p>",
+        link1a: "<i class='fa-solid fa-tower-broadcast'></i> Live Site",
+        link1b: "<i class='fa-solid fa-code'></i> Source Code",
+        link2a: "<i class='fa-solid fa-tower-broadcast'></i> Live Site",
+        link2b: "<i class='fa-solid fa-code'></i> Source Code"
     },
     Swahili: {
         greeting: "Jambo, ninaitwa Dennis",
@@ -29,7 +38,16 @@ const translations = {
         secttech5: "Usambazaji",
         skill1: "Tajriba kiasi kwenye NodeJS",
         skill2: "Tajriba kiasi kwenye BootstrapCSS",
-        skill3: "Tajriba kiasi kwenye React"
+        skill3: "Tajriba kiasi kwenye React",
+        sectproj: "Miradi Yangu",
+        ptitle1: "Tovuti ya Biashara Mtandao ya Kicks.Co",
+        ptitle2: "Wijeti za GitHub",
+        pp1: "Huu ni mfano wa tovuti ya Biashara Mtandao ambayo inahusiana na uuzaji wa viatu, vinavyojulikana kama 'Kicks' kwenye msamiati wa lugha ya misimu ya Kiingereza.<br><br>Ujenzimbele: HTML, CSS, JS <br>Ujenzinyuma: JS, Firestore Firebase",
+        pp2: "Hii ni Huduma isiyo rasmi ya Wijeti ya GitHub inayomruhusu mtu kupachika Wasifu wake wa GitHub na maelezo ya Hifadhi za GitHub kwenye tovuti zao.<br><br>Ujenzimbele: HTML, CSS, JS <br>Ujenzinyuma: JS</p>",
+        link1a: "<i class='fa-solid fa-tower-broadcast'></i> Tovuti ya Moja kwa Moja",
+        link1b: "<i class='fa-solid fa-code'></i> Msimbo Chanzo",
+        link2a: "<i class='fa-solid fa-tower-broadcast'></i> Tovuti ya Moja kwa Moja",
+        link2b: "<i class='fa-solid fa-code'></i> Msimbo Chanzo"
     },
     German: {
         greeting: "Hallo, ich heiße Dennis",
@@ -45,7 +63,16 @@ const translations = {
         secttech5: "Einsatz",
         skill1: "Einige Erfahrung mit NodeJS",
         skill2: "Einige Erfahrung mit BootstrapCSS",
-        skill3: "Einige Erfahrung mit React"
+        skill3: "Einige Erfahrung mit React",
+        sectproj: "Meine Projekte",
+        ptitle1: "Kicks.Co E-Commerce-Website",
+        ptitle2: "GitHub Widgets",
+        pp1: "Dies ist der Prototyp einer E-Commerce-Website, die auf den Verkauf von Turnschuhen spezialisiert ist, die im englischen Slang auch als „Kicks“ bekannt sind.<br><br>Frontend: HTML, CSS, JS <br>Backend: JS, Firestore Firebase",
+        pp2: "Dies ist ein inoffizieller GitHub-Widget-Dienst, der es ermöglicht, Informationen zu GitHub-Profilen und GitHub-Repositorys auf den eigenen Websites einzubetten.<br><br>Frontend: HTML, CSS, JS <br>Backend: JS</p>",
+        link1a: "<i class='fa-solid fa-tower-broadcast'></i> Live-Site",
+        link1b: "<i class='fa-solid fa-code'></i> Quellcode",
+        link2a: "<i class='fa-solid fa-tower-broadcast'></i> Live-Site",
+        link2b: "<i class='fa-solid fa-code'></i> Quellcode"
     }
 };
 
@@ -84,6 +111,15 @@ document.querySelectorAll('.custom-option').forEach(option => {
         document.getElementById('skill1').innerHTML = translations[selectedLanguage].skill1;
         document.getElementById('skill2').innerHTML = translations[selectedLanguage].skill2;
         document.getElementById('skill3').innerHTML = translations[selectedLanguage].skill3;
+        document.getElementById('sectproj').innerHTML = translations[selectedLanguage].sectproj;
+        document.getElementById('ptitle1').innerHTML = translations[selectedLanguage].ptitle1;
+        document.getElementById('ptitle2').innerHTML = translations[selectedLanguage].ptitle2;
+        document.getElementById('pp1').innerHTML = translations[selectedLanguage].pp1;
+        document.getElementById('pp2').innerHTML = translations[selectedLanguage].pp2;
+        document.getElementById('link1a').innerHTML = translations[selectedLanguage].link1a;
+        document.getElementById('link1b').innerHTML = translations[selectedLanguage].link1b;
+        document.getElementById('link2a').innerHTML = translations[selectedLanguage].link2a;
+        document.getElementById('link2b').innerHTML = translations[selectedLanguage].link2b;
 
         // Update nav links
         const navItems = document.querySelectorAll('#nav li a');
